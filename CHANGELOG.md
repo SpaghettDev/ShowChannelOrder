0f1d903a9effe3f0bad54469ac16637ba4148ea4
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2025-04-20 15:22:14 -0400

    feat(inputs): Only show inputs when it makes sense

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.1.0] - 2025-04-20
+
+## Fixed
+
+- Inputs being shown when it doesn't make sense (e.g.: selecting non-triggers)
+
 ## [1.0.0] - 2025-04-20
 
 Initial commit
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
+#include <functional>
+
 #include <Geode/modify/SetGroupIDLayer.hpp>
 
 using namespace geode::prelude;
+
+template <typename T>
+bool areAllObjects(CCArray* arr, std::function<bool(T*)>&& predicate)
+{
+	for (auto c : CCArrayExt<T*>(arr))
+		if (c && !predicate(c)) return false;
+
+	return true;
+}
 
 struct SCOSetGroupIDLayer : geode::Modify<SCOSetGroupIDLayer, SetGroupIDLayer>
 {
 	bool init(GameObject* obj, CCArray* objs)
 	{
-		this->m_showChannelOrder = true;
+		auto isTrigger = [](GameObject* go) {
+			return go->m_classType == GameObjectClassType::Effect;
+		};
+
+		this->m_showChannelOrder = obj
+			? isTrigger(obj)
+			: areAllObjects<GameObject>(objs, std::move(isTrigger));
 
 		return SetGroupIDLayer::init(obj, objs);
 	}
